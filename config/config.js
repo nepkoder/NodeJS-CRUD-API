c2c1e9ee9f1c03646dde79f9b9f15c981f43386c
Sujan Shrestha <nepkoder@gmail.com>
2018-11-29 08:58:33 +0545

    change db config

--- config/config.js
+++ config/config.js
@@ -1,16 +1,18 @@
+// db configure
 var config = {
     PORT: 3000,
     site: 'http://locahost/node-crud-operation/#/',
 
     //database credentials
-
     mongo: {
         hostname: 'localhost',
         port: '27017',
-        // username
         db: 'nodejs-api'
     }
 };
+
+
+// database connection url
 config.mongo.url = 'mongodb://'+config.mongo.hostname+ ':' + config.mongo.port +"/" +config.mongo.db;
 
 module.exports = config;
